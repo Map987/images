a689095b59f96b21254255ef37d5d46557ed934b
Map987 <99967856+Map987@users.noreply.github.com>
2024-04-22 16:16:58 +0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,23 @@
+import requests
+
+# 指定要下载的文件的URL
+url = 'https://images2.penguinrandomhouse.com/cover/tif/9780670786190'
+
+# 指定下载到本地的文件路径
+local_filename = 'download/9780670786190.tif'
+
+try:
+    # 发送HTTP请求
+    response = requests.get(url, stream=True)
+
+    # 检查请求是否成功
+    response.raise_for_status()
+
+    # 以二进制写模式打开文件
+    with open(local_filename, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:  # 过滤掉保持连接的新的块
+                f.write(chunk)
+    print(f'文件已下载到 {local_filename}')
+except requests.exceptions.HTTPError as err:
+    print(f'下载过程中出现错误: {err}')
