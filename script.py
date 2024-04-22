979a15292b88203efbff763dfbd52cc312e53fa3
Map987 <99967856+Map987@users.noreply.github.com>
2024-04-22 17:37:46 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,30 @@
-import requests
 import os
+import requests
 
-def download_file(url, local_filename):
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
+# 文件下载地址
+url = 'https://images2.penguinrandomhouse.com/cover/tif/9780670786190'
 
-# 使用环境变量来设置URL和本地文件名
-url = os.getenv('DOWNLOAD_URL')
-local_filename = os.getenv('LOCAL_FILENAME')
+# 目标文件夹
+folder = 'download'
 
-if url and local_filename:
-    download_file(url, local_filename)
-    print(f"File downloaded: {local_filename}")
-else:
-    print("Environment variables not set.")
+# 确保文件夹存在
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+# 文件名（从URL中提取）
+filename = url.split('/')[-1]
+
+# 完整的文件路径
+file_path = os.path.join(folder, filename)
+
+# 下载文件
+try:
+    response = requests.get(url, stream=True)
+    response.raise_for_status()  # 检查请求是否成功
+
+    with open(file_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+    print(f"文件已下载: {file_path}")
+except requests.exceptions.HTTPError as err:
+    print(f"下载过程中发生错误: {err}")
